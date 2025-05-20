1425b1460a0471a73c8acd6e13d36da56bac66c4
Yan Zhizhong <a2564011261@163.com>
2025-05-20 12:10:17 +0800

    修复请求头中的 Love-Token 错误，改为 X-Love-Token

--- kernel/api/love.ts
+++ kernel/api/love.ts
@@ -16,5 +16,6 @@ module.exports = async (req: VercelRequest, res: VercelResponse) => {
         status: status,
         message: message,
     };
+    res.setHeader('Access-Control-Expose-Headers', 'Love-Token'); // 示例
     res.status(200).json(data);
 }
